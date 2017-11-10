53011238f1988d4b7ae3306ae470d10924418d3a
jruppert <jruppert@ius.edu>
2017-11-09 22:40:50 -0500

    Activity Finished

--- app/src/main/java/org/staysee/forcefitness/MainMenuActivity.java
+++ app/src/main/java/org/staysee/forcefitness/MainMenuActivity.java
@@ -4,13 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
-public class MuscleActivity extends AppCompatActivity {
+public class MainMenuActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_muscle);
+        setContentView(R.layout.activity_main_menu);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
     }
