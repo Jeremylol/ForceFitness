f0e65ddc5680d09bf786117aa3cf3c88279a018a
jruppert <jruppert@ius.edu>
2017-11-21 17:19:04 -0500

    Update- Jeremy

--- app/src/main/java/org/staysee/forcefitness/MainMenuActivity.java
+++ app/src/main/java/org/staysee/forcefitness/MainMenuActivity.java
@@ -1,11 +1,12 @@
 package org.staysee.forcefitness;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +14,12 @@ public class MainMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+    }
+
+    public void toMuscleActivity(View v) {
+        Intent myIntent = new Intent(this, MuscleActivity.class);
+        this.startActivity(myIntent);
+        overridePendingTransition(R.anim.fade_from_left, 0);
     }
 
 }
--- app/src/main/java/org/staysee/forcefitness/MuscleActivity.java
+++ app/src/main/java/org/staysee/forcefitness/MuscleActivity.java
@@ -3,6 +3,7 @@ package org.staysee.forcefitness;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MuscleActivity extends AppCompatActivity {
 
@@ -13,6 +14,18 @@ public class MuscleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_muscle);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            finish();
+        overridePendingTransition(R.anim.fade_from_left, 0);
+        return super.onOptionsItemSelected(item);
+    }
 }
