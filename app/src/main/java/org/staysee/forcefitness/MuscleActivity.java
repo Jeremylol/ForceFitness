b54b10c4659328e75a775cb14890973d75aabbc1
ghfrench <gabefrenchy@gmail.com>
2017-11-26 14:13:37 -0500

    WorkoutActivity progress

--- app/src/main/java/org/staysee/forcefitness/MuscleActivity.java
+++ app/src/main/java/org/staysee/forcefitness/MuscleActivity.java
@@ -3,9 +3,11 @@ package org.staysee.forcefitness;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 
 public class MuscleActivity extends AppCompatActivity {
 
@@ -31,9 +33,36 @@ public class MuscleActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void onClick(View v) throws ClassNotFoundException {
+        switch(v.getId())
+        {
+            case R.id.button0:
+                Button b = (Button)v;
+
+                String buttonText = b.getText().toString();
+                Intent myIntent = new Intent(this, WorkoutActivity.class);
+                myIntent.putExtra("EXTRA_MUSCLE_GROUP", buttonText);
+                this.startActivity(myIntent);
+                overridePendingTransition(R.anim.fade_from_left, 0);
+                break;
+        }
+        /*
+        Button b = (Button)v;
+        String buttonText = b.getText().toString() + "Activity";
+        Log.d("CREATION", buttonText);
+        toWorkoutActivity(v, buttonText);
+        */
+    }
+    /*
+    public void toWorkoutActivity(View v, String b) throws ClassNotFoundException{
+        Intent myIntent = new Intent(this, Class.forName(b));
+        this.startActivity(myIntent);
+        overridePendingTransition(R.anim.fade_from_left, 0);
+    }
+
     public void toWorkoutActivity(View v) {
         Intent myIntent = new Intent(this, WorkoutActivity.class);
         this.startActivity(myIntent);
         overridePendingTransition(R.anim.fade_from_left, 0);
-    }
+    }*/
 }
--- app/src/main/java/org/staysee/forcefitness/WorkoutActivity.java
+++ app/src/main/java/org/staysee/forcefitness/WorkoutActivity.java
@@ -6,12 +6,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.content.Intent;
 import android.view.View;
+import android.util.Log;
 
 public class WorkoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String s = getIntent().getStringExtra("EXTRA_MUSCLE_GROUP");
+        Log.d("yoooooooo", s);
         setContentView(R.layout.activity_workout);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
