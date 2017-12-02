79962db5c809df1cb4f6fb1c89b42a7cbe7644b4
jruppert <jruppert@ius.edu>
2017-12-01 22:18:00 -0500

    Database progress

--- app/src/main/java/org/staysee/forcefitness/DatabaseHelper.java
+++ app/src/main/java/org/staysee/forcefitness/DatabaseHelper.java
@@ -10,11 +10,9 @@ import android.util.Log;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String TAG = "DatabaseHelper";
-
-    private static final String TABLE_NAME = "people_table";
-    private static final String COL1 = "ID";
-    private static final String COL2 = "name";
-
+    private static final String TABLE_NAME = "favorites";
+    private static final String COL0 = "ID";
+    private static final String COL1 = "name";
 
     public DatabaseHelper(Context context) {
         super(context, TABLE_NAME, null, 1);
@@ -23,7 +21,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COL2 + " TEXT)";
+                COL1 + " TEXT)";
         db.execSQL(createTable);
     }
 
@@ -36,7 +34,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public boolean addData(String item) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL2, item);
+        contentValues.put(COL1, item);
 
         Log.d(TAG, "addData: Adding " + item + " to " + TABLE_NAME);
 
@@ -60,8 +58,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public Cursor getItemID(String name) {
         SQLiteDatabase db = this.getWritableDatabase();
-        String query = "SELECT " + COL1 + " FROM " + TABLE_NAME +
-                " WHERE " + COL2 + " = '" + name + "'";
+        String query = "SELECT " + COL0 + " FROM " + TABLE_NAME +
+                " WHERE " + COL1 + " = '" + name + "'";
         Cursor data = db.rawQuery(query, null);
         return data;
     }
@@ -69,10 +67,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void deleteName(int id, String name) {
         SQLiteDatabase db = this.getWritableDatabase();
         String query = "DELETE FROM " + TABLE_NAME + " WHERE "
-                + COL1 + " = '" + id + "'" +
-                " AND " + COL2 + " = '" + name + "'";
+                + COL0 + " = '" + id + "'" +
+                " AND " + COL1 + " = '" + name + "'";
         Log.d(TAG, "deleteName: query: " + query);
         Log.d(TAG, "deleteName: Deleting " + name + " from database.");
         db.execSQL(query);
     }
+
 }
--- app/src/main/java/org/staysee/forcefitness/ExerciseActivity.java
+++ app/src/main/java/org/staysee/forcefitness/ExerciseActivity.java
@@ -7,6 +7,7 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
