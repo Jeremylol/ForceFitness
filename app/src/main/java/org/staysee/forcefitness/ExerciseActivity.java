ee9b5fd86b0f355a5032611e5041475f0a814dbb
jruppert <jruppert@ius.edu>
2018-03-27 17:54:38 -0400

    bugs with favorite fixed

--- app/src/main/java/org/staysee/forcefitness/ExerciseActivity.java
+++ app/src/main/java/org/staysee/forcefitness/ExerciseActivity.java
@@ -78,6 +78,7 @@ public class ExerciseActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
+
         return super.onCreateOptionsMenu(menu);
     }
 
--- app/src/main/java/org/staysee/forcefitness/FavoritesActivity.java
+++ app/src/main/java/org/staysee/forcefitness/FavoritesActivity.java
@@ -10,9 +10,9 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.Guideline;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -52,40 +52,26 @@ public class FavoritesActivity extends AppCompatActivity {
         Guideline guideline = (Guideline) findViewById(R.id.guideline);
         int buttonNum = 0;
         ArrayList<String> favTitles = new ArrayList<>();
-        while (data.moveToNext()) {
-            buttonNum++;
-            favTitles.add(data.getString(1));
-        }
-        Log.d("THIS IS WHAT YOURE", Integer.toString(buttonNum));
-        data.close();
-        Button[] buttons = new Button[buttonNum];
-        buttons[0] = new Button(this);
-        buttons[0].setText(favTitles.get(0));
-        buttons[0].setId(1);
-        buttons[0].setTextSize(35);
-        buttons[0].setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Button button = (Button) v;
-                String buttonText = button.getText().toString();
-                Intent myIntent = new Intent(FavoritesActivity.this, ExerciseActivity.class);
-                myIntent.putExtra("SOURCE", "favorites");
-                myIntent.putExtra("WORKOUT_EXERCISE", buttonText);
-                startActivity(myIntent);
-            }
-        });
-        layout.addView(buttons[0]);
-        set.connect(buttons[0].getId(), ConstraintSet.TOP, guideline.getId(), ConstraintSet.BOTTOM, 0);
-        set.connect(buttons[0].getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0);
-        set.connect(buttons[0].getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0);
-        set.constrainHeight(buttons[0].getId(), 200);
-        set.applyTo(layout);
 
-        for (int i = 1; i < buttons.length; i++) {
-            buttons[i] = new Button(this);
-            buttons[i].setText(favTitles.get(i));
-            buttons[i].setId(i + 1);
-            buttons[i].setTextSize(35);
-            buttons[i].setOnClickListener(new View.OnClickListener() {
+        if (data.getCount() == 0) {
+            data.close();
+            TextView textview = (TextView) findViewById(R.id.textView2);
+            textview.setText("You have no favorites");
+            textview.setTextSize(35);
+        } else {
+            while (data.moveToNext()) {
+                buttonNum++;
+                favTitles.add(data.getString(1));
+            }
+            Log.d("THIS IS WHAT YOURE", Integer.toString(buttonNum));
+            data.close();
+
+            Button[] buttons = new Button[buttonNum];
+            buttons[0] = new Button(this);
+            buttons[0].setText(favTitles.get(0));
+            buttons[0].setId(1);
+            buttons[0].setTextSize(35);
+            buttons[0].setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     Button button = (Button) v;
                     String buttonText = button.getText().toString();
@@ -95,14 +81,38 @@ public class FavoritesActivity extends AppCompatActivity {
                     startActivity(myIntent);
                 }
             });
-            layout.addView(buttons[i]);
-            set.connect(buttons[i].getId(), ConstraintSet.TOP, buttons[i - 1].getId(), ConstraintSet.BOTTOM, 0);
-            set.connect(buttons[i].getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0);
-            set.connect(buttons[i].getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0);
-            set.constrainHeight(buttons[i].getId(), 200);
+            layout.addView(buttons[0]);
+            set.connect(buttons[0].getId(), ConstraintSet.TOP, guideline.getId(), ConstraintSet.BOTTOM, 0);
+            set.connect(buttons[0].getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0);
+            set.connect(buttons[0].getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0);
+            set.constrainHeight(buttons[0].getId(), 200);
             set.applyTo(layout);
+
+            for (int i = 1; i < buttons.length; i++) {
+                buttons[i] = new Button(this);
+                buttons[i].setText(favTitles.get(i));
+                buttons[i].setId(i + 1);
+                buttons[i].setTextSize(35);
+                buttons[i].setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Button button = (Button) v;
+                        String buttonText = button.getText().toString();
+                        Intent myIntent = new Intent(FavoritesActivity.this, ExerciseActivity.class);
+                        myIntent.putExtra("SOURCE", "favorites");
+                        myIntent.putExtra("WORKOUT_EXERCISE", buttonText);
+                        startActivity(myIntent);
+                    }
+                });
+                layout.addView(buttons[i]);
+                set.connect(buttons[i].getId(), ConstraintSet.TOP, buttons[i - 1].getId(), ConstraintSet.BOTTOM, 0);
+                set.connect(buttons[i].getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0);
+                set.connect(buttons[i].getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0);
+                set.constrainHeight(buttons[i].getId(), 200);
+                set.applyTo(layout);
+            }
         }
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
